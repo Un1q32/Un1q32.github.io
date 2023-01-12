f256f78e4c8072e36180319caeb122f03bad297c
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-12 03:02:02 -0500

    move more stuff

--- update.sh
+++ update.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 cd "${0%/*}" || exit 1
 (
-cd armv6 || exit 1
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -fk Packages
-)
-(
-cd armv7 || exit 1
+cd arm32 || exit 1
 dpkg-scanpackages -m ./debs > Packages
 bzip2 -fk Packages
 )
