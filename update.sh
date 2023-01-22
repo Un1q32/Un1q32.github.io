134ab7df07544a4049fc25fe202e5bbac9f5f524
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-22 08:04:52 -0500

    cancel support for multiple architectures

--- update.sh
+++ update.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 cd "${0%/*}" || exit 1
-(
-cd arm32 || exit 1
 dpkg-scanpackages -m ./debs > Packages
 bzip2 -fk Packages
-)
-(
-cd arm64 || exit 1
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -fk Packages
-)
