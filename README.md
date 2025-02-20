79458d21f961d70b4b6a81fc782d4be42cb03e58
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-20 16:32:57 -0500

    reorganize

--- README.md
+++
@@ -1,5 +0,0 @@
-https://Un1q32.github.io/
-
-http://cydia.uniq.gay/
-
-To request a package, make a github issue.
--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
 cd "${0%/*}"
-dpkg-scanpackages -m ./debs > Packages
-gzip -fkn Packages
-bzip2 -fk Packages
+for arch in armv6-ios i386-macos x86_64-macos; do
+    (
+    cd "$arch"
+    dpkg-scanpackages -m ./debs > Packages 2> /dev/null
+    dpkg-scanpackages -m ../all >> Packages 2> /dev/null
+    gzip -fkn Packages
+    bzip2 -fk Packages
+    ) &
+done
+wait
+printf 'Done!\n'
