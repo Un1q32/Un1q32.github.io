2b031ae8945dc6643d6a3b0ffa4ff07ce3f319df
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-22 18:45:22 -0500

    update names and such

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ for arch in armv6-ios i386-macos x86_64-macos; do
     (
     cd "$arch"
     dpkg-scanpackages -m ./debs > Packages 2> /dev/null
-    dpkg-scanpackages -m ../all >> Packages 2> /dev/null
     gzip -fkn Packages
     bzip2 -fk Packages
     ) &
